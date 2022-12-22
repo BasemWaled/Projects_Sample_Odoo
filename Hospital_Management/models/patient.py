7526f4ea4ef878900a8e4708125a32659107bfae
BASEM WALED <86613419+BasemWaled@users.noreply.github.com>
2022-12-22 15:43:18 +0200

    Add files via upload

--- Hospital_Management/models/patient.py
+++ Hospital_Management/models/patient.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models
+from datetime import date
 
 
 class Hospitalpatient(models.Model):
@@ -7,8 +8,19 @@ class Hospitalpatient(models.Model):
     _description = "Hospital patient"
 
     name = fields.Char(string='Name', tracking=True)
+    date_of_birth = fields.Date(string='Date of Birth')
     mobile = fields.Char(string='Mobile', tracking=True)
-    age = fields.Integer(string='Age', tracking=True)
+    age = fields.Integer(string='Age', compute='_compute_age', tracking=True)
     gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender", tracking=True, default='female')
     description = fields.Text(string='Description', tracking=True)
     active = fields.Boolean(string='active', default=True)
+    appointment_id = fields.Many2one('hospital.appointment', string='Appointment')
+
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+        for rec in self:
+            today = date.today()
+            if rec.date_of_birth:
+                rec.age = today.year - rec.date_of_birth.year
+            else:
+                rec.age = 1
--- Link_all_Project/models/links.py
+++ Link_all_Project/models/links.py
@@ -5,10 +5,10 @@ class Linksystem(models.Model):
     _name = "project.name"
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    Project_Name = fields.Char(string='Project_Name', tracking=True)
-    Url = fields.Text(string='Url', tracking=True)
-    UserName = fields.Char(string='UserName', tracking=True)
-    Password = fields.Char(string='Password', tracking=True)
-    Project_Type = fields.Selection([('community', 'Community'), ('enterprise', 'Enterprise')], tracking=True)
+    project_Name = fields.Char(string='Project_Name', tracking=True)
+    url = fields.Text(string='Url', tracking=True)
+    userName = fields.Char(string='UserName', tracking=True)
+    password = fields.Char(string='Password', tracking=True)
+    project_Type = fields.Selection([('community', 'Community'), ('enterprise', 'Enterprise')], tracking=True)
     active = fields.Boolean(string='active', default=True)
     version = fields.Selection([('10','10'), ('11','11'),('12','12'), ('13','13'),('14','14'), ('15','15'),('16','16')], tracking=True)
