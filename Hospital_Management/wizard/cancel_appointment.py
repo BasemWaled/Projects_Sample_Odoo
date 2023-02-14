f5b952aa62240354c0c0ee43d59b086bac51ce0e
BASEM WALED <bassemwaled35@gmail.com>
2023-02-14 21:29:24 +0200

    act

---
+++ Hospital_Management/wizard/cancel_appointment.py
@@ -0,0 +1,8 @@
+from odoo import fields, models
+
+
+class CancelAppointmentWizard(models.TransientModel):
+    _name = 'cancel.appointment.wizard'
+    _description = 'Cancel Appointment Wizard'
+
+
