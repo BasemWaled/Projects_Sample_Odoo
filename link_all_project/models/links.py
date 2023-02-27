b52fa946155a10261a8596a78a17bf84ecf64537
BASEM WALED <bassemwaled35@gmail.com>
2023-02-27 22:53:29 +0200

    [add]basem

--- link_all_project/models/links.py
+++ link_all_project/models/links.py
@@ -6,12 +6,13 @@ class Linksystem(models.Model):
     _inherit = "project.project"
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    project_Name = fields.Char(string='Project_Name', tracking=True)
-    url = fields.Text(string='Url', tracking=True)
-    userName = fields.Char(string='UserName', tracking=True)
-    password = fields.Char(string='Password', tracking=True)
-    project_Type = fields.Selection([('community', 'Community'), ('enterprise', 'Enterprise')], tracking=True)
+    project_Name = fields.Many2one('project.project', string='Project Name', tracking=True, required=True)
+    url = fields.Text(string='Url', tracking=True, required=True)
+    userName = fields.Char(string='UserName', tracking=True, required=True)
+    password = fields.Char(string='Password', tracking=True, required=True)
+    project_Type = fields.Selection([('community', 'Community'), ('enterprise', 'Enterprise')], tracking=True,
+                                    required=True)
     active = fields.Boolean(string='active', default=True)
     version = fields.Selection(
         [('10', '10'), ('11', '11'), ('12', '12'), ('13', '13'), ('14', '14'), ('15', '15'), ('16', '16')],
-        tracking=True)
+        tracking=True, required=True)
