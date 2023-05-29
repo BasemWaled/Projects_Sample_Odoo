533217e115448bfc834e1813013d19a8c7aef072
Basem Walid <bassemwaled35@gmail.com>
2023-05-30 03:58:20 +0400

    [add]

---
+++ whatsaapLoop/__manifest__.py
@@ -0,0 +1,19 @@
+
+{
+    'name': 'Pay K_Matrix',
+    'version': '15.0',
+    'category': 'manging',
+    'summary': 'Pay Mobile',
+    'description': """ testing pay """,
+    'author': 'K_Matrix',
+    'depends': ['account'],
+    'data': [],
+    'demo': [],
+    'sequence': '-98',
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'assets': {
+    },
+
+}
