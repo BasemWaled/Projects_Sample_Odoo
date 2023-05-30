6149992481f2c18be664165c02c4f2253b8ca038
BASEM WALED <86613419+BasemWaled@users.noreply.github.com>
2023-05-30 15:02:23 +0400

    [add]

---
+++ whatsaapLoop/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'WhatsApp Loop',
+    'version': '15.0',
+    'summary': """External API for CRM leads and customers Whatsloop """,
+    'description': """
+        This module provides an external API to create CRM leads and customers and phone and opprtiounty.
+    """,
+    'category': 'developers',
+    'author': 'K_MATRIX & Basem Walid',
+    'website': "https://www.linkedin.com/in/basem-walid600/",
+    'depends': ['base', 'crm'],
+    'data': [
+        'views/crm_lead_view.xml'
+    ],
+    'demo': [],
+    'sequence': '-88',
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'assets': {
+    },
+
+}
