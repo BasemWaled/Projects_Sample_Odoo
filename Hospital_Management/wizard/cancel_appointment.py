309e70254d7a7d4dfd21e5c84df6bf7a5ce78e9c
BASEM WALED <bassemwaled35@gmail.com>
2023-08-19 16:38:52 +0300

    [add]

--- Hospital_Management/wizard/cancel_appointment.py
+++ Hospital_Management/wizard/cancel_appointment.py
@@ -17,11 +17,10 @@ class CancelAppointmentWizard(models.TransientModel):
 
     def action_cancel(self):
         cancel_day = self.env['ir.config_parameter'].get_param('Hospital_Management.cancel_day')
+        print("cancel_day", cancel_day)
         allow_today = self.appointment_id.booking_date - relativedelta.relativedelta(days=int(cancel_day))
-        if cancel_day != 0 and allow_today < date.today():
+        if allow_today < date.today():
             raise ValidationError(_("Sorry, this cancellation is not allowed for this booking"))
-        self.appointment_id.state = 'cancel'
-        return {'type': 'ir.actions.client', 'tag': 'reload'}
         # if self.appointment_id.booking_date == fields.Date.today():
         #     raise ValidationError(_("Sorry, cancellation is not allowed on the same"))
         # self.appointment_id.state = 'cancel'
