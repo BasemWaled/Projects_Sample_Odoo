4f844be5606607ab9df53b3990ddf3b6741cd8f5
BASEM WALED <bassemwaled35@gmail.com>
2023-08-19 16:50:51 +0300

    [add]

--- Hospital_Management/wizard/cancel_appointment.py
+++ Hospital_Management/wizard/cancel_appointment.py
@@ -1,6 +1,5 @@
 import datetime
 from datetime import date
-
 from dateutil import relativedelta
 from odoo import fields, models, api, _
 from odoo.exceptions import ValidationError
@@ -19,12 +18,13 @@ class CancelAppointmentWizard(models.TransientModel):
         cancel_day = self.env['ir.config_parameter'].get_param('Hospital_Management.cancel_day')
         print("cancel_day", cancel_day)
         allow_today = self.appointment_id.booking_date - relativedelta.relativedelta(days=int(cancel_day))
-        if allow_today < date.today():
+        if cancel_day != 0 and allow_today < date.today():
             raise ValidationError(_("Sorry, this cancellation is not allowed for this booking"))
+        self.appointment_id.state = 'cancel'
+        return {'type': 'ir.actions.client', 'tag': 'reload'}
         # if self.appointment_id.booking_date == fields.Date.today():
         #     raise ValidationError(_("Sorry, cancellation is not allowed on the same"))
         # self.appointment_id.state = 'cancel'
-        # return
 
     @api.model
     def default_get(self, fields):
