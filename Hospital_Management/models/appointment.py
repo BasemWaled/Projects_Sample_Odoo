30ab505aa900d57cbe0f07ff7e742f401654cbb4
BASEM WALED <bassemwaled35@gmail.com>
2023-08-30 16:00:40 +0300

    [add]

--- Hospital_Management/models/appointment.py
+++ Hospital_Management/models/appointment.py
@@ -1,3 +1,4 @@
+import random
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 
@@ -7,7 +8,7 @@ class HospitalAppointment(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Appointment"
     _rec_name = 'patient_id'
-    #_rec_name = 'name'
+    # _rec_name = 'name'
     _order = 'id desc'
 
     name = fields.Char(string='Sequence', default='new', readonly=True)
@@ -24,9 +25,10 @@ class HospitalAppointment(models.Model):
         [('draft', 'Draft'), ('in_consultation', 'In Consultation'), ('done', 'Done'), ('cancel', 'Cancelled')],
         default='draft', string="Status", required=True, tracking=True)
     doctor_id = fields.Many2one('res.users', string='Doctor', tracking=True)
-    operation_id = fields.Many2one('hospital.operation', string='Operation', tracking=True)
     appointment_pharmacy_ines = fields.One2many('appointment.pharmacy.lines', 'appointment_id', string='Pharmacy Lines')
     hide_sales_price = fields.Boolean(string="Hide Sales Price")
+    operation_id = fields.Many2one('hospital.operation', string='Operation', tracking=True)
+    progress = fields.Integer('Progress', compute='_compute_progress')
 
     @api.model
     def create(self, vals):
@@ -67,6 +69,31 @@ class HospitalAppointment(models.Model):
         if self.state != 'draft':
             raise ValidationError(_("you can't delete because this appointment is done"))
         return super(HospitalAppointment, self).unlink()
+
+    # @api.depends('state')
+    # def _compute_progress(self):
+    #     for rec in self:
+    #         if rec.state == 'draft':
+    #             progress = 25
+    #         elif rec.state == 'in_consultation':
+    #             progress = 50
+    #         elif rec.state == 'done':
+    #             progress = 100
+    #         else:
+    #             progress = 0
+    #         rec.progress = progress
+    @api.depends('state')
+    def _compute_progress(self):
+        for rec in self:
+            if rec.state == 'draft':
+                progress = random.randrange(0, 25)
+            elif rec.state == 'in_consultation':
+                progress = random.randrange(25, 99)
+            elif rec.state == 'done':
+                progress = 100
+            else:
+                progress = 0
+            rec.progress = progress
 
 
 class AppointmentPharmacyLines(models.Model):
