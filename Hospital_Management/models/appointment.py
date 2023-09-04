4075c2726191aef9ee1cd058e00732d034e03c87
BASEM WALED <bassemwaled35@gmail.com>
2023-09-04 18:01:30 +0300

    [add]

--- Hospital_Management/models/appointment.py
+++ Hospital_Management/models/appointment.py
@@ -29,7 +29,8 @@ class HospitalAppointment(models.Model):
     hide_sales_price = fields.Boolean(string="Hide Sales Price")
     operation_id = fields.Many2one('hospital.operation', string='Operation', tracking=True)
     progress = fields.Integer('Progress', compute='_compute_progress')
-    duration = fields.Float(string='Duration')
+    duration = fields.Float("Duration")
+
 
     @api.model
     def create(self, vals):
