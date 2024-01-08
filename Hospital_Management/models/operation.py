3edf01a24881d75a8164c5f6b56d31f6dd14b7f6
BASEM WALED <86613419+BasemWaled@users.noreply.github.com>
2024-01-09 00:36:39 +0200

    add

--- Hospital_Management/models/operation.py
+++ Hospital_Management/models/operation.py
@@ -4,13 +4,15 @@ from odoo import api, fields, models
 class HospitalOperation(models.Model):
     _name = "hospital.operation"
     _description = "Hospital Operation"
-    _log_access = False
     _rec_name = 'operation_name'
+    _log_access = False
+    _order = 'sequence,id'
 
     doctor_id = fields.Many2one('res.users', string='Doctor')
     operation_name = fields.Char(string='Name')
     reference_record = fields.Reference(selection=[('hospital.patient', 'Patient'),
                                                    ('hospital.appointment', 'Appointment')], string='Record')
+    sequence = fields.Integer(string='Sequence', default=10)
 
     @api.model
     def name_create(self, name):
