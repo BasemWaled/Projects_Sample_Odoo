f378c6fa6110d5e4f6d465db2b8640a393274092
BASEM WALED <86613419+BasemWaled@users.noreply.github.com>
2024-01-09 02:08:13 +0200

    Update wb_sample.js

--- odoo_learn/static/src/js/wb_sample.js
+++ odoo_learn/static/src/js/wb_sample.js
@@ -4,9 +4,17 @@ odoo.define("odoo_learn.WBSampleButton", function(require){
     const PosComponent = require("point_of_sale.PosComponent");
     const ProductScreen = require("point_of_sale.ProductScreen");
     const Registries = require("point_of_sale.Registries");
+    const { useListener } = require("@web/core/utils/hooks");
 
     class WBSampleButton extends PosComponent {
 
+        setup(){
+            super.setup();
+            useListener("click", this.wb_sample_button_click)
+        }
+        wb_sample_button_click(){
+            console.log("Hello this is a sample button")
+        }
     }
 
     WBSampleButton.template = "WBSampleButton";
