a81b6551c1a8cf20a97d18b923c746a17a18ced3
BASEM WALED <86613419+BasemWaled@users.noreply.github.com>
2024-01-10 15:48:45 +0200

    add

---
+++ owl_playground/static/src/playground.js
@@ -0,0 +1,17 @@
+/** @odoo-module **/
+
+import { Component, useState } from "@odoo/owl";
+
+export class Playground extends Component {
+    static template = "owl_playground.playground";
+
+    static template = "owl_playground.Counter";
+
+    setup() {
+        this.state = useState({ value: 0 });
+    }
+
+    increment() {
+        this.state.value++;
+    }
+}
